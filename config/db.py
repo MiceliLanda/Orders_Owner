724756d9f3b2b667ae43c58b8e236d76d16a4265
Jhonathan Miceli Landa <jhonathanmicelilanda@MacBook-Air-de-Jhonathan.local>
2022-07-26 22:19:22 -0500

    sessions

--- config/db.py
+++ config/db.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(url="mysql+pymysql://ufood:ufood2022@ufoodb.c2ppwslmpouq.us-east-1.rds.amazonaws.com:3306/ufood", echo=True)
 meta = MetaData()
 conn = engine.connect()
+Session = sessionmaker(bind=engine,autocommit=True)
+session = Session()
--- routes/owner.py
+++ routes/owner.py
@@ -1,6 +1,6 @@
 
 from fastapi import APIRouter
-from config.db import conn
+from config.db import conn,session
 from models.order import tableOrder, tableOrderDetails, tableMenu, tableSaucer, tableShop, tableOwner,tableUser
 from fastapi import status
 from fastapi.responses import JSONResponse
@@ -13,7 +13,7 @@ orderRoute = APIRouter()
 async def rechargeCredits(shops_id: int):
     try:
         query = select([tableOrder.c.id, tableOrder.c.status, tableOrder.c.user_id, tableShop.c.name, tableUser.c.name, tableSaucer.c.name]).select_from(tableOrder).join(tableOrderDetails,tableOrder.c.id == tableOrderDetails.c.order_id).join(tableSaucer,tableSaucer.c.id==tableOrderDetails.c.saucer_id).join(tableMenu,tableSaucer.c.menu_id == tableMenu.c.id).join(tableShop,tableMenu.c.shop_id == tableShop.c.id).join(tableUser,tableOrder.c.user_id == tableUser.c.id).where(tableOrder.c.status == 0).where(tableShop.c.id == shops_id)
-        result = conn.execute(query).fetchall()
+        result = session.execute(query).fetchall()
         data = []
         for row in result:
             data.append({"id_orden":row[0],"status":row[1],"user_id":row[2],"shop_name":row[3],"user_name":row[4],"saucer_name":row[5]})
@@ -25,7 +25,7 @@ async def rechargeCredits(shops_id: int):
 async def preparingOrder(orden_id: int):
     try:
         query = tableOrder.update().where(tableOrder.c.id == orden_id).values(status=1)
-        conn.execute(query)
+        session.execute(query)
         return {"status":"Orden en proceso"}
     except Exception as e:
         return {"Error":str(e)}      
@@ -34,7 +34,7 @@ async def preparingOrder(orden_id: int):
 async def orderCompleted(orden_id: int):
     try:
         query = tableOrder.update().where(tableOrder.c.id == orden_id).values(status=2)
-        conn.execute(query)
+        session.execute(query)
         return {"status":"Puedes pasar a recoger la orden"}
     except Exception as e:
         return {"Error":str(e)}
@@ -43,7 +43,7 @@ async def orderCompleted(orden_id: int):
 async def getOrdersPushNotifications(shop_id: int):
     try:
         query = select([tableOrder.c.id, tableOrder.c.status, tableOrder.c.user_id, tableShop.c.name, tableUser.c.name, tableSaucer.c.name]).select_from(tableOrder).join(tableOrderDetails,tableOrder.c.id == tableOrderDetails.c.order_id).join(tableSaucer,tableSaucer.c.id==tableOrderDetails.c.saucer_id).join(tableMenu,tableSaucer.c.menu_id == tableMenu.c.id).join(tableShop,tableMenu.c.shop_id == tableShop.c.id).join(tableUser,tableOrder.c.user_id == tableUser.c.id).where(tableOrder.c.status == 2).where(tableShop.c.id == shop_id)
-        result = conn.execute(query).fetchall()
+        result = session.execute(query).fetchall()
         data = []
         for row in result:
             data.append({"id_orden":row[0],"status":row[1],"user_id":row[2],"shop_name":row[3],"user_name":row[4],"saucer_name":row[5]})
